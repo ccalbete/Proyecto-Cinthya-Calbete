6ff5c70fa2df8892ae5e1e316ec2b977298318d7
ccalbete <ccalbete10@gmail.com>
2021-09-12 19:44:36 -0300

    autofill categories and fixedexpenses from javascript!

---
+++ home/script.js
@@ -0,0 +1,96 @@
+let fixedExpenses = [
+    {
+        name: "Booking",
+        isPending: true
+    },
+    {
+        name: "Credit card",
+        isPending: true
+    },
+    {
+        name: "University",
+        isPending: false
+    },
+    {
+        name: "English course",
+        isPending: false
+    },
+    {
+        name: "Gym",
+        isPending: true
+    },
+    {
+        name: "Internet",
+        isPending: false
+    }
+]
+
+let catagories = [
+    {
+        name: "Food",
+        spent: "15000"
+    },
+    {
+        name: "Outings",
+        spent: "2300"
+    },
+    {
+        name: "Clothes",
+        spent: "1600"
+    },
+    {
+        name: "Transportation",
+        spent: "500"
+    },
+    {
+        name: "Cleaning",
+        spent: "1800"
+    },
+    {
+        name: "Pharmacy",
+        spent: "600"
+    }
+]
+
+function fillPendingsList(){
+    let listElements = document.getElementById("pendingsList");
+
+    for(let i=0; i < fixedExpenses.length; i++){
+        const listItem = document.createElement("li");
+        const iconItem =  document.createElement("i");
+
+        listElements.appendChild(listItem);
+        listItem.textContent = fixedExpenses[i].name + " ";
+        listItem.appendChild(iconItem);
+        iconItem.className="far";
+        iconItem.classList.toggle("fa-circle", fixedExpenses[i].isPending);
+        iconItem.classList.toggle("fa-check-circle", !fixedExpenses[i].isPending);
+    }
+}
+
+function fillSummarySection(){
+    const BLUE = "blue";
+    const SKYBLUE = "skyblue";
+    let lastColor = SKYBLUE;
+    const summarySection = document.getElementById("summarySection");
+    
+    for(let i=0; i < catagories.length; i++){
+        let categoryLabel = document.createElement("label");
+        let spentElement = document.createElement("p");
+        let categoryNameElement = document.createElement("p");
+        summarySection.appendChild(categoryLabel)
+        if(lastColor === SKYBLUE){
+            categoryLabel.className = "backgroundBlue";
+            lastColor = BLUE;
+        }else{
+            categoryLabel.className = "backgroundSkyblue";
+            lastColor = SKYBLUE;
+        }
+        spentElement.textContent = catagories[i].spent;
+        spentElement.className = "spent";
+        categoryLabel.appendChild(spentElement);
+        categoryNameElement.textContent = catagories[i].name;
+        categoryLabel.appendChild(categoryNameElement)
+
+    }
+}
