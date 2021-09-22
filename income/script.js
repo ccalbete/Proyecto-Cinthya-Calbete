ae5a7a0debe9499f57fbe5f4a34595455d478299
ccalbete <ccalbete10@gmail.com>
2021-09-22 01:00:28 -0300

    fill automatically lists add income, payment modes doesn't works

---
+++ income/script.js
@@ -0,0 +1,34 @@
+let reasons = [
+    "Salary",
+    "Leftover last month"
+]
+
+let paymentModes = [
+    "Cash",
+    "BROU debit card",
+    "Santader debit card",
+    "Santander credit card",
+    "Itau credit card",
+    "Food card",
+]
+
+let reasonListElement = document.getElementById("reasons")
+let amountElement = document.getElementById("amount")
+let paymentmodeListElement = document.getElementById("paymentModes")
+let saveButtonElement = document.getElementById("saveButton")
+
+
+
+function fillListsValues(){
+    fillList(reasons, reasonListElement);
+    fillList(paymentModes, paymentModesListElement);
+}
+
+
+function fillList(listValues, listElement){
+    for(let i=0; i < listValues.length; i++){
+        let option = document.createElement("option");
+        listElement.appendChild(option);
+        option.value = listValues[i];
+    }
+}
