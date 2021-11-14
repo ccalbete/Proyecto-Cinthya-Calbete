ff91e74e21549602fbdf5c2281a779b31679a1da
ccalbete <ccalbete10@gmail.com>
2021-11-14 00:34:59 -0300

    get categories and fixed expenses from backend, close #5

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -30,49 +30,80 @@ function homeCreateAndFillElements() {
 
 function fillPendingsList() {
 
-    user.categories.forEach(category => {
-        if(category.isFixed) {
+    getFixedExpensesByUser().then((userFixedExpenses) => {
+        userFixedExpenses.forEach(fixedExpense => {
             const listItem = document.createElement("li");
-            listItem.textContent = category.name;
-            if(category.spent > 0) {
+            listItem.textContent = fixedExpense.name;
+            if (fixedExpense.spent > 0) {
                 paidExpensesElement.appendChild(listItem);
             } else {
                 pendingExpensesElement.appendChild(listItem);
             }
-        }
-    });
+        });
+    })
 }
 
 function fillSummarySection() {
     const BLUE = "blue";
     const SKYBLUE = "skyblue";
     let lastColor = SKYBLUE;
-    const categoriesToShow = user.categories.filter(category => !category.isFixed);
 
-    for(let i=0; i < categoriesToShow.length; i++) {
+    getCategoriesByUser().then((userCategories) => {
+        for (let i = 0; i < userCategories.length; i++) {
 
-        let categoryItemsGrouper = document.createElement("div");
-        let spentElement = document.createElement("p");
-        let categoryNameElement = document.createElement("p");
-        summarySectionElement.appendChild(categoryItemsGrouper)
+            let categoryItemsGrouper = document.createElement("div");
+            let spentElement = document.createElement("p");
+            let categoryNameElement = document.createElement("p");
+            summarySectionElement.appendChild(categoryItemsGrouper)
 
-        if(lastColor === SKYBLUE) {
-            categoryItemsGrouper.className = "backgroundBlue";
-            lastColor = BLUE;
-        } else {
-            categoryItemsGrouper.className = "backgroundSkyblue";
-            lastColor = SKYBLUE;
+            if (lastColor === SKYBLUE) {
+                categoryItemsGrouper.className = "backgroundBlue";
+                lastColor = BLUE;
+            } else {
+                categoryItemsGrouper.className = "backgroundSkyblue";
+                lastColor = SKYBLUE;
+            }
+            spentElement.textContent = "$" + userCategories[i].spent;
+            spentElement.className = "spent";
+            categoryItemsGrouper.appendChild(spentElement);
+            categoryNameElement.textContent = userCategories[i].name;
+            categoryItemsGrouper.appendChild(categoryNameElement)
+
         }
-        spentElement.textContent = "$"+categoriesToShow[i].spent;
-        spentElement.className = "spent";
-        categoryItemsGrouper.appendChild(spentElement);
-        categoryNameElement.textContent = categoriesToShow[i].name;
-        categoryItemsGrouper.appendChild(categoryNameElement)
+    })
 
-    }
+
 }
 
-function showTitleCurrentMonth(){
+function showTitleCurrentMonth() {
     const currentDate = new Date()
-    titleDateElement.textContent = months[currentDate.getMonth()] + " " +currentDate.getFullYear()
+    titleDateElement.textContent = months[currentDate.getMonth()] + " " + currentDate.getFullYear()
+}
+
+function getCategoriesByUser() {
+    return fetch(url + "/categories/" + user, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token"),
+        }
+    }).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        return response.userCategories;
+    })
+}
+
+function getFixedExpensesByUser() {
+    return fetch(url + "/fixedExpenses/" + user, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token"),
+        }
+    }).then(function (response) {
+        return response.json();
+    }).then(function (response) {
+        return response.userFixedExpenses;
+    })
 }
